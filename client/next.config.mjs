9733ee238884d30ed6fad15d734c3a4ef1bfd412
PetheanRaj-A-Mitrah <petheanraj.a@mitrahsoft.com>
2025-07-23 18:05:12 +0530

    Updated the next js config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,10 +2,9 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: "http",
-                hostname: "127.0.0.1",
-                port: "1337",
-                pathname: "/uploads/**/*",
+                protocol: "https",
+                hostname: "worthy-cheese-16c170eb03.strapiapp.com",
+                pathname: "/**",
             },
         ],
     },
